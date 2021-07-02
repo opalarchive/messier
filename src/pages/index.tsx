aadfd6cd446389b8474507310d2a3218b4d403d7
Amol Rama <43121460+AmoleR@users.noreply.github.com>
2021-07-01 18:07:39 -0700

    Adding everything else

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <div>Hi</div>;
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div>Hi</div>
+    </>
+  );
 }
