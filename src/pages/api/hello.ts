91f4c00f4ba1d63f3593e0f2f46f196a41c8078b
Dennis <proofprogram@gmail.com>
2021-07-05 18:23:43 -0700

    categories + some random command stuff

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,65 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { writeFile } from "fs/promises";
+import path from "path";
 
-type Data = {
-  name: string;
+const data = {
+  general: {
+    help: {
+      description:
+        "Shows the contest bot help message, or if a command is provided, information about the command.",
+      aliases: ["h", "listcmds", "listcommands"],
+      usage: "&help [command]",
+      arguments: [
+        {
+          name: "command",
+          type: "string",
+          optional: true,
+        },
+      ],
+      cooldown: 5000,
+      subcommands: {
+        dm: {
+          description:
+            "Gets the main help command sent as a DM, or if a command is provided, information about the command.",
+          usage: "&help dm [command]",
+          arguments: [
+            {
+              name: "command",
+              type: "string",
+              optional: true,
+            },
+          ],
+          cooldown: 10000,
+        },
+      },
+    },
+  },
+  settings: {
+    spamy: {
+      description: "Show some information about the bot",
+      aliases: ["i", "hi", "hello"],
+      usage: "&info",
+      cooldown: 5000,
+    },
+  },
+  groupsolve: {
+    rama: {
+      description: "Show some information about the bot",
+      aliases: ["i", "hi", "hello"],
+      usage: "&info",
+      cooldown: 5000,
+    },
+  },
 };
 
-export default function handler(
+export default async function handler(
   _req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<string>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  await writeFile(
+    path.join(process.cwd(), "./commands.json"),
+    JSON.stringify(data, null, 2)
+  );
+  res.status(200).send("Done");
 }
